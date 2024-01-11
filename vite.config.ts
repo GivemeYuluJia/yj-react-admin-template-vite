75143f49fb1694dba16b15e9eca3bb0d55612622
GivemeYuluJia <1095778225@qq.com>
2024-01-11 16:55:07 +0800

    feat: adjust eslint and prettierrc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode.mode, process.cwd());
+  console.log("env is:", env, "vite-config", mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src")
+      }
+    },
+    plugins: [react()]
+  };
+});
